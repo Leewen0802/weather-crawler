7ff6c3ed4dee9311e47deced0f1d632b8fcbaf60
Clyde [李文] <leewen0802888@gmail.com>
2020-12-12 14:57:18 +0800

    Update FooterComponent.js

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -8,7 +8,6 @@ function FooterComponent() {
       <p>
         <a href="https://github.com/cscolabear/weather-crawler/">weather-crawler</a> | puppeteer, ci, travis, nodejs
       </p>
-      <p>Coding by Cola</p>
 
       <div className="github-area">
         {/* <div className="nes-balloon is-rounded-dark from-left ">
@@ -19,9 +18,6 @@ function FooterComponent() {
             <i className="nes-icon star"></i> Please~~~
           </p>
         </div> */}
-        <a href="https://github.com/cscolabear/weather-crawler" target="_blank" rel="noopener noreferrer">
-          <i className="nes-octocat animate"></i>
-        </a>
       </div>
     </footer>
   );
